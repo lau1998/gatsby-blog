f1f984a8c4a2d53c17b4a9a9230cac0b12f8ecbd
Laucher <196663624@qq.com>
2023-04-08 01:37:26 +0800

    更新
    更新

--- src/components/Skills/content.js
+++ src/components/Skills/content.js
@@ -2,51 +2,51 @@ const links = [
   {
     label: 'Vue'
   },
-  {
-    label: 'Javascript'
-  },
-  {
-    label: 'Typescript'
-  },
-  {
-    label: 'HTML'
-  },
-  {
-    label: 'CSS'
-  },
-  {
-    label: 'Sass'
-  },
-  {
-    label: 'Less'
-  },
   // {
-  //   label: 'Nodejs'
+  //   label: 'Javascript'
   // },
-  {
-    label: 'Npm'
-  },
-  {
-    label: 'Yarn'
-  },
-  {
-    label: 'Git'
-  },
-  {
-    label: 'React'
-  },
-  {
-    label: 'Gatsby'
-  },
-  {
-    label: 'Github'
-  },
-  {
-    label: 'Gitee'
-  },
-  {
-    label: 'StackOverflow'
-  },
+  // {
+  //   label: 'Typescript'
+  // },
+  // {
+  //   label: 'HTML'
+  // },
+  // {
+  //   label: 'CSS'
+  // },
+  // {
+  //   label: 'Sass'
+  // },
+  // {
+  //   label: 'Less'
+  // },
+  // // {
+  // //   label: 'Nodejs'
+  // // },
+  // {
+  //   label: 'Npm'
+  // },
+  // {
+  //   label: 'Yarn'
+  // },
+  // {
+  //   label: 'Git'
+  // },
+  // {
+  //   label: 'React'
+  // },
+  // {
+  //   label: 'Gatsby'
+  // },
+  // {
+  //   label: 'Github'
+  // },
+  // {
+  //   label: 'Gitee'
+  // },
+  // {
+  //   label: 'StackOverflow'
+  // },
 ];
 
 export default links;
