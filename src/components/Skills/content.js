1c573e2f83da9d86f716f738242c9b67f386e580
Laucher <196663624@qq.com>
2023-04-08 01:34:15 +0800

    去掉nodejs技能logo
    去掉nodejs技能logo

--- src/components/Skills/content.js
+++ src/components/Skills/content.js
@@ -2,51 +2,51 @@ const links = [
   {
     label: 'Vue'
   },
-  // {
-  //   label: 'Javascript'
-  // },
-  // {
-  //   label: 'Typescript'
-  // },
-  // {
-  //   label: 'HTML'
-  // },
-  // {
-  //   label: 'CSS'
-  // },
-  // {
-  //   label: 'Sass'
-  // },
-  // {
-  //   label: 'Less'
-  // },
+  {
+    label: 'Javascript'
+  },
+  {
+    label: 'Typescript'
+  },
+  {
+    label: 'HTML'
+  },
+  {
+    label: 'CSS'
+  },
+  {
+    label: 'Sass'
+  },
+  {
+    label: 'Less'
+  },
   // {
   //   label: 'Nodejs'
   // },
-  // {
-  //   label: 'Npm'
-  // },
-  // {
-  //   label: 'Yarn'
-  // },
-  // {
-  //   label: 'Git'
-  // },
-  // {
-  //   label: 'React'
-  // },
-  // {
-  //   label: 'Gatsby'
-  // },
-  // {
-  //   label: 'Github'
-  // },
-  // {
-  //   label: 'Gitee'
-  // },
-  // {
-  //   label: 'StackOverflow'
-  // },
+  {
+    label: 'Npm'
+  },
+  {
+    label: 'Yarn'
+  },
+  {
+    label: 'Git'
+  },
+  {
+    label: 'React'
+  },
+  {
+    label: 'Gatsby'
+  },
+  {
+    label: 'Github'
+  },
+  {
+    label: 'Gitee'
+  },
+  {
+    label: 'StackOverflow'
+  },
 ];
 
 export default links;
