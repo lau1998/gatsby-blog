82f5c087b5d18a3dbeb1f0817fbb57111a6b5abf
Laucher <196663624@qq.com>
2023-04-08 01:29:18 +0800

    更新技能2

--- src/components/Skills/content.js
+++ src/components/Skills/content.js
@@ -2,39 +2,39 @@ const links = [
   {
     label: 'Vue'
   },
-  {
-    label: 'Javascript'
-  },
-  {
-    label: 'Typescript'
-  },
-  {
-    label: 'HTML'
-  },
-  {
-    label: 'CSS'
-  },
-  {
-    label: 'Sass'
-  },
-  {
-    label: 'Less'
-  },
-  {
-    label: 'Nodejs'
-  },
-  {
-    label: 'Npm'
-  },
-  {
-    label: 'Yarn'
-  },
-  {
-    label: 'Git'
-  },
-  {
-    label: 'React'
-  },
+  // {
+  //   label: 'Javascript'
+  // },
+  // {
+  //   label: 'Typescript'
+  // },
+  // {
+  //   label: 'HTML'
+  // },
+  // {
+  //   label: 'CSS'
+  // },
+  // {
+  //   label: 'Sass'
+  // },
+  // {
+  //   label: 'Less'
+  // },
+  // {
+  //   label: 'Nodejs'
+  // },
+  // {
+  //   label: 'Npm'
+  // },
+  // {
+  //   label: 'Yarn'
+  // },
+  // {
+  //   label: 'Git'
+  // },
+  // {
+  //   label: 'React'
+  // },
   // {
   //   label: 'Gatsby'
   // },
